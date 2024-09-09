f8cf1fc857fe6e5ba7bdcae0653aa35510156208
gabrielnavas <gabrielnavas@protonmail.com>
2024-09-09 18:16:16 -0300

    add home and custom logout page

---
+++ app/(marketing)/logout/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { FC, useEffect } from "react";
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { signOut, useSession } from "next-auth/react";
+
+interface ILogoutProps { };
+
+const Logout: FC<ILogoutProps> = (props) => {
+  const { data, status } = useSession();
+
+  const route = useRouter();
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      route.push('/login')
+    }
+  }, [status])
+
+  return (
+    <div className="w-full h-dvh flex flex-col justify-center align-middle">
+      <div className="flex justify-center  text-black-800 text-2xl text-bold mb-2 ">
+        {status === 'loading' && 'Waiting...'}
+        {data && data.user && (
+          `${data?.user?.name}, are you sure you want to leave?'`
+        )}
+      </div>
+      <div className="flex justify-center gap-2">
+        <Button className="w-[17%]" onClick={() => signOut()}>Yes</Button>
+        <Button className="w-[40%]" variant='outline' onClick={() => route.back()}>No</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Logout;
--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -5,11 +5,8 @@ import Link from "next/link";
 import { Button, buttonVariants } from "@/components/ui/button";
 
 export default function AuthButton({ page }: { page: string }) {
-  const { data: session, status } = useSession();
+  const { status } = useSession();
   const isAuthenticated = status === "authenticated";
-
-  console.log(session);
-  
 
   return (
     <>
