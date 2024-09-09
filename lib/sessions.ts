fb38026cd9c4c2123203bcf8d4b19d566f61ffde
gabrielnavas <gabrielnavas@protonmail.com>
2024-09-09 17:55:33 -0300

    add auth provider

--- lib/sessions.ts
+++ lib/sessions.ts
@@ -1,12 +1,18 @@
 import {getServerSession} from 'next-auth'
 import { authOptions } from './auth';
 
-export async function getCurrentUser(): Promise<any> {
+type User = {
+  name: string 
+  email: string 
+  image: string
+}
+
+export async function getCurrentUser(): Promise<User | null> {
   const session = await getServerSession(authOptions);
   
   if(session === null || session.user === null) {
     return null
   }
 
-  return session.user;
+  return session.user as User;
 }
